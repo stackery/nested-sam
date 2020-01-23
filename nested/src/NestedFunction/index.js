d6d22e5e44dfeac499684cf9c723ba136e3378b5
Matthew Bradburn <bradburn.m@gmail.com>
2020-01-23 13:44:43 -0800

    initial commit

---
+++ nested/src/NestedFunction/index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event, context) => {
+  // Log the event argument for debugging and for use in local development.
+  console.log(JSON.stringify(event, undefined, 2));
+
+  console.log('Here in my TopLevel function');
+
+  return {};
+};
